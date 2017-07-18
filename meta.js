fcff31f4cbf286f4edbc6e838f50cb0b06710977
meng <mling@sina.com>
2017-07-18 15:14:51 +0800

    配置

--- meta.js
+++ meta.js
@@ -87,11 +87,11 @@ module.exports = {
   "filters": {
     ".eslintrc.js": "lint",
     ".eslintignore": "lint",
-    "config/test.env.js": "unit || e2e",
-    "test/unit/**/*": "unit",
-    "build/webpack.test.conf.js": "unit",
-    "test/e2e/**/*": "e2e",
-    "src/router/**/*": "router"
+    "*/config/test.env.js": "unit || e2e",
+    "*/test/unit/**/*": "unit",
+    "*/build/webpack.test.conf.js": "unit",
+    "*/test/e2e/**/*": "e2e",
+    "*/src/router/**/*": "router"
   },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm install --registry=https://registry.npm.taobao.org\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
